583d845c78b8f6df6133237ac11a3dafb21e285e
Bartosz Janicki <janicki.bartosz.0@gmail.com>
2025-01-12 19:41:10 +0100

    Add json dump
    Signed-off-by: Bartosz Janicki <janicki.bartosz.0@gmail.com>

--- edge_detection/edge_detection/edge_detection.py
+++ edge_detection/edge_detection/edge_detection.py
@@ -3,48 +3,79 @@ import time
 from os import listdir
 from os.path import join, isfile
 from . import filters
+from . import image_result
 import cv2 as cv
+import json
 
 
-def save_after_filter(path, img, name, time, performance_data_file):
+def save_after_filter(path, img, name, time):
     cv.imwrite(path, img)
     info = f"{name}: execution time = {time:.8f}s"
-    print(info)
-    performance_data_file.write(f"{info}\n")
+    # print(info)
 
-def apply_filter(filter_name, filter_func, img, paths, performance_file):
+
+def apply_filter(filter_name, filter_func, img, paths):
     start = time.monotonic()
     filtered_img = filter_func(img)
     end = time.monotonic()
     execution_time = end - start
-    save_after_filter(paths[filter_name], filtered_img, filter_name, execution_time, performance_file)
+    save_after_filter(paths[filter_name], filtered_img, filter_name, execution_time)
+    return execution_time
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 def run():
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    image_id = 0
+    results = []
     input_dirs = ["forest", "mountain"]
     for input_dir in input_dirs:
         input_path = os.path.join(BASE_DIR, "input", input_dir)
         images = [f for f in listdir(input_path) if isfile(join(input_path, f))]
         for image in images:
+
+            image_id += 1
+            img_original_path = os.path.join(input_path, image)
             img_name = image
 
-            paths = {
-                "img": os.path.join(input_path, img_name),
-                **{key: os.path.join("..", "output", input_dir, key, img_name) for key in filters.FILTERS.keys()},
-                "performance_data": os.path.join("..", "output", input_dir, "performance", img_name + ".txt")
-            }
+            paths = {"img": os.path.join(input_path, img_name),
+                     **{key: os.path.join("..", "output", input_dir, key, img_name) for key in
+                        filters.FILTERS.keys()},
+                     "json_dump": os.path.join("..", "output", input_dir)}
 
             for key in paths:
                 os.makedirs(os.path.dirname(paths[key]), exist_ok=True)
 
             img = cv.imread(paths["img"], cv.IMREAD_GRAYSCALE)
+            height, width = img.shape
 
-            with open(paths["performance_data"], "w") as performance_data_file:
-                img_info = f"{img_name}, resolution: {img.shape[1]}x{img.shape[0]}, execution time of specific methods are listed below."
-                performance_data_file.write(f"{img_info}\n")
-                print(img_info)
+            execution_times = {}
+            # Apply each filter (defined in filters.py)
+            for filter_name, filter_func in filters.FILTERS.items():
+                execution_times[filter_name] = apply_filter(filter_name, filter_func, img, paths)
 
-                # Apply each filter (defined in filters.py)
-                for filter_name, filter_func in filters.FILTERS.items():
-                    apply_filter(filter_name, filter_func, img, paths, performance_data_file)
+            results.append(image_result.ImageResult(
+                id=image_id,
+                original_path=img_original_path,
+                is_high_resolution=False,  # Placeholder
+                is_ai_generated=False,  # Placeholder
+                is_gauss_noise=False,  # Placeholder
+                is_salt_and_pepper_noise=False,  # Placeholder
+                roberts_path=paths["roberts"],
+                prewitt_path=paths["prewitt"],
+                sobel_path=paths["sobel"],
+                robinson_path=paths["robinson"],
+                laplace_path=paths["laplacian"],
+                canny_path=paths["canny"],
+                time_roberts=execution_times["roberts"],
+                time_prewitt=execution_times["prewitt"],
+                time_sobel=execution_times["sobel"],
+                time_robinson=execution_times["robinson"],
+                time_laplace=execution_times["laplacian"],
+                time_canny=execution_times["canny"],
+                width=width,
+                height=height
+            ))
+
+            output_json_path = os.path.join(BASE_DIR, paths["json_dump"], "results.json")
+            with open(output_json_path, "w") as json_file:
+                json.dump([result.to_dict() for result in results], json_file, indent=4)
---
+++ edge_detection/edge_detection/image_result.py
@@ -0,0 +1,56 @@
+from dataclasses import asdict, dataclass
+from PIL import Image
+
+
+@dataclass
+class ImageResult:
+    id: int
+    original_path: str
+    is_high_resolution: bool
+    is_ai_generated: bool
+    is_gauss_noise: bool
+    is_salt_and_pepper_noise: bool
+    roberts_path: str
+    prewitt_path: str
+    sobel_path: str
+    robinson_path: str
+    laplace_path: str
+    canny_path: str
+    time_roberts: int
+    time_prewitt: int
+    time_sobel: int
+    time_robinson: int
+    time_laplace: int
+    time_canny: int
+    width: int
+    height: int
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "ImageResult":
+        image = Image.open(data["original_path"])
+        width, height = image.size
+        return cls(
+            id=data["id"],
+            original_path=data["original_path"],
+            is_high_resolution=data["is_high_resolution"],
+            is_ai_generated=data["is_ai_generated"],
+            is_gauss_noise=data["is_gauss_noise"],
+            is_salt_and_pepper_noise=data["is_salt_and_pepper_noise"],
+            roberts_path=data["roberts_path"],
+            prewitt_path=data["prewitt_path"],
+            sobel_path=data["sobel_path"],
+            robinson_path=data["robinson_path"],
+            laplace_path=data["laplace_path"],
+            canny_path=data["canny_path"],
+            time_roberts=data["time_roberts"],
+            time_prewitt=data["time_prewitt"],
+            time_sobel=data["time_sobel"],
+            time_robinson=data["time_robinson"],
+            time_laplace=data["time_laplace"],
+            time_canny=data["time_canny"],
+            width=width,
+            height=height
+        )
+
+    def to_dict(self) -> dict:
+        return asdict(self)
